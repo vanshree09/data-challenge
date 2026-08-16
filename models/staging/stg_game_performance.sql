a739c2d838bd17a49b44e94199a4f2a62f6967b7
vanshree09@gmail.com <vanshree09@gmail.com>
2026-08-16 05:25:23 +0000

    NA

--- models/staging/stg_game_performance.sql
+++ models/staging/stg_game_performance.sql
@@ -13,8 +13,13 @@ SELECT
     CAST(fp AS integer) AS fp,
     CAST(turnover_sum AS numeric(12,2)) AS turnover_sum,
     CAST(gmp_sum AS numeric(12,2)) AS gmp_sum,
-    CAST(games_played_sum AS bigint) AS games_played_sum    
-FROM {{ source('raw', 'game_performance') }}
+    CAST(games_played_sum AS bigint) AS games_played_sum,
+    ingestion_id,
+    active_from_timestamp,
+    active_to_timestamp,
+    active_status   
+FROM {{ source('raw', 'game_performance_audit') }}
+WHERE active_status = 'Y'
 
 {% if is_incremental() %}
 WHERE CAST(bus_date AS date) > (SELECT max(bus_date) FROM {{ this }})
